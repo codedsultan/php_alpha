096fabdc29ff114518cdaafd0734e6e5f1fedf83
codesultan <codesultan369@gmail.com>
2024-08-19 13:03:58 +0100

    login and registration

--- routes.php
+++ routes.php
@@ -3,3 +3,10 @@
 $router->get('/', 'index.php');
 $router->get('/about', 'about.php');
 $router->get('/contact', 'contact.php');
+
+$router->get('/register', 'registration/create.php')->only('guest');
+$router->post('/register', 'registration/store.php');
+
+$router->get('/login', 'sessions/create.php')->only('guest');
+$router->post('/session', 'sessions/store.php')->only('guest');
+$router->delete('/session', 'sessions/destroy.php')->only('auth');
